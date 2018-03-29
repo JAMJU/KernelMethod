f6aa0da040d02e30e81855ba415385494fae3720
JAMJU <juliette.milletviolette@wanadoo.fr>
2018-03-29 19:19:19 +0200

    add last modif

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from logistic_regression import logistic_kernel_regression, compute_label
 from kernel_creation import convert_spectral_kernel_quad, convert_spectral_kernel_quint, convert_spectral_kernel_trig
 from kernel_creation import convert_acid_kernel, convert_acid_quad, convert_mismatch_lev, convert_lect_trig, get_mismatch_dict
-from kernel_creation import get_correspondances, convert_mismatch_dico, get_full_corres
+from kernel_creation import get_correspondances, convert_mismatch_dico, get_full_corres, convert_encode
 from kernel_creation import compute_test_matrix, compute_K_matrix, convert_lect_acid, compute_K_gaussian
 from read_fn import read_csv_file_label, read_csv_file_data, save_label, save_data_converted
 from SVM import SVM, svm_compute_label
@@ -27,7 +27,7 @@ def is_pos_def(x):
     return np.all(np.linalg.eigvals(x) > 0)
 
 ## Parameters
-lamb_log = 0.000001
+lamb_log = 0.0000001
 lamb_svm = 0.00001
 sigma = 0.8
 add_param = 10.**(-10)
@@ -39,7 +39,7 @@ if mis_lev:
 
 mis_dic = False
 size_seq = 6
-nb_mis = 2
+nb_mis = 1
 beg = 0
 if mis_dic:
     dict_corres = get_correspondances(list_seq_id, nb_mis, list_letters)
@@ -53,14 +53,14 @@ if mis_dic_full:
 ##
 list_labels_log = []
 list_labels_svm = []
-for name in ["0", "1", "2"]:
+for name in [ "0", "1","2"]:
     print ("beginning loading of the data")
     # Training data
     sequences = read_csv_file_data("data/Xtr"+ name+ ".csv")
     #list_converted = convert_spectral_kernel_trig(sequences, list_seq_id)
     #list_converted = convert_spectral_kernel_quad(sequences, list_quad)
-
     list_converted = convert_spectral_kernel_quint(sequences, list_quint)
+    #list_converted = convert_spectral_kernel_quint(sequences, list_quint)
     #list_converted = convert_acid_kernel(sequences, dico_acid)
     #list_converted = convert_acid_quad(sequences, dico_acid, list_quad
 
@@ -68,10 +68,18 @@ for name in ["0", "1", "2"]:
     #list_converted = convert_lect_trig(sequences, list_seq_id, beg)
     #list_converted = convert_lect_acid(sequences, dico_acid, beg)
     #list_converted = convert_mismatch_dico(sequences, dict_corres,list_mis_corres, list_seq_id)
+    #list_converted = convert_encode(sequences, list_letters)
     training = np.asarray(list_converted, dtype = float)
+
     # to avoid huge values and to save time for the logistic regression :
-    sm =  np.sum(training, axis= 1)
+    sm = np.sum(training, axis= 1)
     training = training/sm[0]
+    mean =  np.mean(training, axis= 0)
+
+    training = training - mean
+
+    #vst = np.std(training, axis= 0)
+    #training = training / vst
     #save_data_converted("spectral_kernel/Xtr"+ name+ ".csv", training)
 
     # label training data
@@ -99,21 +107,40 @@ for name in ["0", "1", "2"]:
     #list_converted_test = convert_lect_trig(sequences_test, list_seq_id, beg )
     #list_converted_test = convert_lect_acid(sequences_test, dico_acid, beg)
     #list_converted_test = convert_mismatch_dico(sequences_test, dict_corres,list_mis_corres, list_seq_id)
+    #list_converted_test = convert_encode(sequences, list_letters)
     testing = np.asarray(list_converted_test, dtype = float)
     # to avoid huge values and to save time for the logistic regression :
     testing = testing/sm[0]
+    testing = testing - mean
 
+    #testing = testing/ vst
+    # param for each dataset:
+    """if name=="0":
+        lamb_svm = 0.000008
+        add_param = 10. ** (-10)
+
+    if name=="1":
+        lamb_svm = 0.00001
+        add_param = 10.**(-10)
+
+    if name == "2":
+        lamb_svm = 0.000005
+        add_param=10.**(-9)"""
+
+    if name=="2":
+        add_param = 10**(-9)
 
 
     print ("data loaded")
 
     # Computing the kernel
     print ("beginning computing K")
-    # K = compute_K_matrix(training)
-    # add = add_param*np.identity(K.shape[0])
-    # K_add = K + add # to make it positive definite
-    K = compute_K_gaussian(training, sigma)
-    K_add = K
+
+    K = compute_K_matrix(training)
+    add = add_param*np.identity(K.shape[0])
+    K_add = K + add # to make it positive definite
+    #K = compute_K_gaussian(training, sigma)
+    #K_add = K
     print(K)
     print("K shape", K.shape)
     print(is_pos_def(K_add))
@@ -139,5 +166,6 @@ for name in ["0", "1", "2"]:
     list_labels_svm = list_labels_svm + labels_test
 
 
-#save_label(0, list_labels_log,"results/LKR_acidquad_0-000001.csv" )
-save_label(0, list_labels_svm,"results/GaussianSVM_1_quint_10-4.csv" )
+save_label(0, list_labels_svm,"results/SVM-quint-centered-mixed.csv" )
+
+
